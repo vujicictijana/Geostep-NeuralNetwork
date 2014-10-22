2436d7fdcaace9ef07a3b8b58435fbb469637bf9
vujicictijana <vujicic.tijana@gmail.com>
2014-10-22 13:03:27 +0200

    neural network web

--- Recommendation/bin/recommendation/routes/home.clj
+++ Recommendation/bin/recommendation/routes/home.clj
@@ -1,7 +1,8 @@
 (ns recommendation.routes.home
   (:require[compojure.core :refer :all]
-           [recommendation.views.layout :as layout])
-  (:import  [java.io File]
+           [recommendation.views.layout :as layout]
+           [recommendation.neural-network :as neural-network])
+  (:import [java.io File]
            [org.apache.mahout.cf.taste.impl.model.file FileDataModel]
            [org.apache.mahout.cf.taste.impl.neighborhood ThresholdUserNeighborhood]
            [org.apache.mahout.cf.taste.impl.similarity PearsonCorrelationSimilarity]
@@ -17,7 +18,8 @@
     (def data-list (.recommend recommender user number))))
 
 (defn prepare-item-data[item]
-   (clojure.string/join  ["<item id= \"" (.getItemID item) "\" value=\"" (.getValue item) "\" />"]))
+   (clojure.string/join  
+     ["<item id= \"" (.getItemID item) "\" value=\"" (.getValue item) "\" />"]))
 
 
 (defn prepare-xml-data[lista]
@@ -25,27 +27,21 @@
        lista))
 
 
-
+(defn is-game-relevant [game-id] (neural-network/get-relevant game-id))
 (defn prepare-xml[lista] 
- (str  "<?xml version=\"1.0\" encoding=\"utf-8\"?><items>" (clojure.string/join (prepare-xml-data lista)) "</items>"))
+ (str  "<?xml version=\"1.0\" encoding=\"utf-8\"?><items>" 
+       (clojure.string/join (prepare-xml-data lista)) 
+       "</items>"))
 
 
-
-
-;; test
-
-;(println (prepare-xml data-list))
-
-;(defn parse-int [s]
-;   (Integer. (re-find  #"\d+" s )))
-
-
-
-(defn home [user number file] (do (recommendation (Integer. user) (Integer. number) file )
-               (prepare-xml data-list)))
+(defn home [user number file] 
+  (do 
+    (recommendation (Integer. user) (Integer. number) file )
+    (prepare-xml data-list)))
 
 (defroutes home-routes
-  (GET "/home" [user number file] (home user number file)))
+  (GET "/home" [user number file] (home user number file))
+  (GET "/neural-network" [game-id] (is-game-relevant  game-id)))
 
 
 
