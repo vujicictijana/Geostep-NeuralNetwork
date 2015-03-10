b450e9243c2b3f7d90a7dc23cd15da4ca4b5a499
vujicictijana <vujicic.tijana@gmail.com>
2015-03-10 16:36:59 +0100

    paketi imena

--- Recommendation/bin/recommendation/views/layout.clj
+++
@@ -1,9 +0,0 @@
-(ns recommendation.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
-
-(defn common [& body]
-  (html5
-    [:head
-     [:title "Welcome to geostep-clojure"]
-     (include-css "/css/screen.css")]
-    [:body body]))
