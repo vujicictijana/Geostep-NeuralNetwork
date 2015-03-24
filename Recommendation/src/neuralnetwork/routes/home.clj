ef4ef75126fe2646773ad4941e7826b0ca76e8ee
vujicictijana <vujicic.tijana@gmail.com>
2015-03-24 16:34:01 +0100

    XML 2

--- Recommendation/src/neuralnetwork/routes/home.clj
+++ Recommendation/src/neuralnetwork/routes/home.clj
@@ -7,25 +7,23 @@
   (:import [java.io File]))
 
 
-(defn is-game-relevant 
-  [game-id] 
-  (let [path (str "http://192.168.10.109:81/ReturnGame.aspx?gameNumber=" game-id  "&username=petar")]
-    (neural-network/get-relevant game-id path)))
-
-(defn build-xml[game-id] 
-  (str "<?xml version=\"1.0\" encoding=\"utf-8\"?> <relevant>" (is-game-relevant game-id) "</relevant>"))
-
-
 (defn recommendation 
   [user number file] 
   (do  (recommendation/recommendation (Integer. user) (Integer. number) file )
     (recommendation/prepare-xml neuralnetwork.recommendation/data-list)))
 
+(defn is-game-relevant 
+  [game-id] 
+  (let [path (str "http://192.168.10.109:81/ReturnGame.aspx?gameNumber=" game-id  "&username=petar")]
+    (neural-network/get-relevant game-id path)))
+
+(defn build-xml[txt] 
+  (str "<?xml version=\"1.0\" encoding=\"utf-8\"?> <relevant>" txt "</relevant>"))
 
 (defroutes home-routes  
   (GET "/test-xml" [] (resp/resource-response "/resources/ReturnGameTest.xml"))
   (GET "/home" [user number file] (recommendation user number file))
-  (GET "/neural-network" [game-id] (build-xml game-id)))
+  (GET "/neural-network" [game-id] (build-xml (is-game-relevant game-id))))
   ;;(GET "/documentation" [] (resp/resource-response "../resources/index.html")))
 
 
